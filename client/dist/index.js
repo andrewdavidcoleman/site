8b5a05efaadda9892f9969ce259df65e90689db6
Andrew Coleman <andrewdavidcoleman@gmail.com>
2020-01-23 19:53:04 -0500

    made slide down nav menu that appears when scroll past intro section, added submit button to contact form

--- client/dist/index.js
+++ client/dist/index.js
@@ -3,8 +3,9 @@
 document.getElementById('main').addEventListener("scroll", showMenu)
 
 function showMenu() {
-  console.log('pancakes');
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  console.log('main scrolltop: ' + document.getElementById('main').scrollTop);
+  console.log('height: ' + $(window).height());
+  if (document.getElementById('main').scrollTop > $(window).height()) {
       document.getElementById("scrollMenu").style.top = "0";
   } else {
       document.getElementById("scrollMenu").style.top = "-100%";
